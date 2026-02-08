8ef454b6eb9f7d2e5fd956812b6d1b47a0d15905
Fernando Guillén Suárez <fguillen.mail@gmail.com>
2026-02-08 12:40:49 +0100

    Add Umami events on outside links

---
+++ js/umami.js
@@ -0,0 +1,9 @@
+(() => {
+  const name = 'outbound-link-click';
+  document.querySelectorAll('a').forEach(a => {
+    if (a.host !== window.location.host && !a.getAttribute('data-umami-event')) {
+      a.setAttribute('data-umami-event', name);
+      a.setAttribute('data-umami-event-url', a.href);
+    }
+  });
+})();
