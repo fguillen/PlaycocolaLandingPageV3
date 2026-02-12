e55802fdb8bf258fb915b513f14a0d45ee688075
Fernando Guillén Suárez <fguillen.mail@gmail.com>
2026-02-12 22:39:30 +0100

    Featured in section added to index.html, and outbound link click tracking added to umami.js.

--- js/umami.js
+++ js/umami.js
@@ -2,7 +2,6 @@
   const name = 'outbound-link-click';
   document.querySelectorAll('a').forEach(a => {
     if (a.host !== window.location.host && !a.getAttribute('data-umami-event') && !a.getAttribute('data-fancybox')) {
-      console.log(`Adding Umami event tracking to ${a.href}`);
       a.setAttribute('data-umami-event', name);
       a.setAttribute('data-umami-event-url', a.href);
     }
